d3e0b3009b56aee07b0ae82b2f9a26a0ccd84c14
Mel O'Hagan <mel@budibase.com>
2022-12-16 11:45:19 +0000

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nikita-input-mask
+A custom text field plugin for Budibase that allows you to provide an input mask. 
+Adapted from: https://github.com/xnimorz/svelte-input-mask
+
+# Description
+Mask inputs for credit cards, phone numbers, etc
+
+Find out more about [Budibase](https://github.com/Budibase/budibase).
+
+## Instructions
+
+To build your new  plugin run the following in your Budibase CLI:
+```
+budi plugins --build
+```
+
+You can also re-build everytime you make a change to your plugin with the command:
+```
+budi plugins --watch
+```
+
---
+++ src/Component.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { getContext, onDestroy } from "svelte"
+  import MaskInput from "./MaskInput.svelte"
+
+  export let field
+  export let label
+  export let mask = "0000-000000-00000"
+
+  const component = getContext("component")
+  const formContext = getContext("form")
+  const formStepContext = getContext("form-step") 
+      
+  const formApi = formContext?.formApi
+  $: formStep = formStepContext ? $formStepContext || 1 : 1
+  $: formField = formApi?.registerField(field, "string", "", false, null, formStep)
+
+  const { styleable } = getContext("sdk")
+
+  let fieldApi;
+  let fieldState; 
+
+  $: unsubscribe = formField?.subscribe((value) => {
+    fieldState = value?.fieldState;
+    fieldApi = value?.fieldApi;
+  });
+
+  onDestroy(() => {
+    fieldApi?.deregister()
+    unsubscribe?.()
+  })
+
+  const fieldGroupContext = getContext("field-group")
+  const labelPos = fieldGroupContext?.labelPosition || "above"
+
+  $: labelClass = labelPos === "above" ? "" : `spectrum-FieldLabel--${labelPos}`
+</script>
+
+<div use:styleable={$component.styles}>
+  {#if !formContext}
+    <div class="placeholder">Mask input needs to be wrapped in a form</div>
+  {:else}
+  <div class="spectrum-Form-item">
+    <label
+      class:hidden={!label}
+      for={fieldState?.fieldId}
+      class={`spectrum-FieldLabel spectrum-FieldLabel--sizeM spectrum-Form-itemLabel ${labelClass}`}
+    >
+      {label || " "}
+    </label>
+  </div>
+  <div class="spectrum-Textfield">
+    <MaskInput 
+      on:change={(e) => {
+        fieldApi?.setValue(e.detail?.inputState?.visibleValue)
+        console.log("DEET ", e.detail)
+        console.log("TYPE ", typeof e.detail)
+      }} 
+      alwaysShowMask 
+      maskChar="_" 
+      {mask} 
+    />
+  </div>
+  {/if}
+</div>
+
+<style>
+  label {
+    white-space: nowrap;
+  }
+  label.hidden {
+    padding: 0;
+  }
+  .spectrum-Form-itemField {
+    position: relative;
+    width: 100%;
+  }
+  .spectrum-FieldLabel--right,
+  .spectrum-FieldLabel--left {
+    padding-right: var(--spectrum-global-dimension-size-200);
+  }
+</style>
